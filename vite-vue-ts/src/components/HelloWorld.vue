dd8fbb57b0b8501ec3e243aab0a4c13e03ade3c8
James Jackson <james.ogge@gmail.com>
2024-02-09 16:38:54 +0000

    show input and reversed output

--- vite-vue-ts/src/components/HelloWorld.vue
+++ vite-vue-ts/src/components/HelloWorld.vue
@@ -5,12 +5,20 @@ import board from "../board";
 defineProps<{ msg: string }>()
 
 const count = ref(0)
-let boardMessage = ref("");
+const boardMessage = ref("");
+const boardInput = ref("Hello, Breadboard!");
 
 onMounted(async () => {
-  const { output } = await board({ message: "Hello, Breadboard!" });
+  const { output } = await board({ message: boardInput.value });
   boardMessage.value = output as string;
 })
+
+const onInputChanged = async (event: Event) => {
+  boardInput.value = (event.target as HTMLInputElement).value;
+  const { output } = await board({ message: boardInput.value });
+  boardMessage.value = output as string;
+}
+
 </script>
 
 <template>
@@ -23,7 +31,7 @@ onMounted(async () => {
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
+  <input :value="boardInput" @input="onInputChanged" />
   <p>Board Output: {{ boardMessage }}</p>
   
   <p class="read-the-docs">Click on the Vite, Vue and Breadboard logos to learn more</p>

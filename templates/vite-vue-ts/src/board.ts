0a87352a6f34157cc5c731a618b262d087eae04b
Joseph Mearman <joseph@mearman.co.uk>
2024-02-11 18:13:34 +0000

    format svelte and vue templates

--- templates/vite-vue-ts/src/board.ts
+++ templates/vite-vue-ts/src/board.ts
@@ -1,12 +1,12 @@
 import { board, code } from "@google-labs/breadboard";
 
-const reversed = code<{ text: string }>(({ text }) => {
+const reversed = code<{ text: string; }>(({ text }) => {
 	const reversed = text.split("").reverse().join("");
-	return {reversed};
-})
+	return { reversed };
+});
 
-export default board<{ message: string }>(({ message }, { output }) => {
-	const {reversed: reversedMessage} = reversed({ text: message });
+export default board<{ message: string; }>(({ message }, { output }) => {
+	const { reversed: reversedMessage } = reversed({ text: message });
 	const renamedOutput = reversedMessage.as("output").to(output());
 	return renamedOutput;
 });

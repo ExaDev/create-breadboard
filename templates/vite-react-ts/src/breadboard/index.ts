8ff353e167b4d420ed2d954d299588ea714ec6de
Joseph Mearman <joseph@mearman.co.uk>
2024-02-13 09:56:36 +0000

    move react directory

--- templates/vite-react-ts/src/breadboard/index.ts
+++ templates/vite-react-ts/src/breadboard/index.ts
@@ -1,9 +1,9 @@
 import { board } from "@google-labs/breadboard";
 
-const myBoard = board<{ message: string }>(({ message }, { output }) => {
+const myBoard = board<{ message: string; }>(({ message }, { output }) => {
 	const renamedOutput = message.as("output").to(output());
 	return renamedOutput;
-  });
+});
 
-export { myBoard };
-export { myBoard as default };
+export { myBoard as default, myBoard };
+

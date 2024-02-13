a8ee7544d82089e299ee803acb30e9a84c30bfa1
Joseph Mearman <joseph@mearman.co.uk>
2024-02-13 11:28:05 +0000

    Refactor responseValueIsValid function to include optional name and print parameters

--- src/index.ts
+++ src/index.ts
@@ -69,10 +69,10 @@ const packageJson: {
 );
 const packageVersion = packageJson.version;
 
-function responseValueIsValid(value: any): boolean {
+function responseValueIsValid(value: any, name?: string, print = false): boolean {
 	const result = value && typeof value == "string" && value.length > 0;
-	if (!result) {
-		console.error("Invalid value:", value);
+	if (!result && print) {
+		console.error("Invalid value", print ? name : undefined, value);
 	}
 	return value;
 }
